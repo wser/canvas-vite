77dd6a9b37441c4f85de1501b5c54faf245ddc4b
wser <w.web.dev@gmail.com>
2021-11-08 14:36:30 +0100

    removed canvas background

--- src/components/Can.vue
+++ src/components/Can.vue
@@ -2,144 +2,106 @@
 import { ref, onUpdated, onMounted } from 'vue'
 import Sketch from 'sketch-js'
 
-let myCanvas = ref(null)
-
-let setCanvas = function(w, h, t = this) {
-  t.w = innerWidth
-  t.h = innerHeight
-  t.ctx = null
-  
-  t.setContext = function(ctx) {
-    t.ctx = ctx
-  }
-}
-
-
-let size = { w: 200, h: 400 }
-
-let nrg = new setCanvas(size.w, size.h)
-
 function parts(){
 
-// General Variables
-let particles = [];
+  // General Variables
+  let particles = [];
 
-let sketch = Sketch.create();
-sketch.mouse.x = sketch.width / 2;
-sketch.mouse.y = sketch.height / 2;
-sketch.strokeStyle = 'hsla(200, 50%, 50%, .4)';
-sketch.globalCompositeOperation = 'lighter'; //'luminosity';
+  let sketch = Sketch.create();
+  sketch.mouse.x = sketch.width / 2;
+  sketch.mouse.y = sketch.height / 2;
+  sketch.strokeStyle = 'hsla(200, 50%, 50%, .4)';
+  sketch.globalCompositeOperation = 'lighter'; //'luminosity';
 
-// Particle Constructor
-let Particle = function () {
-  this.x = random(sketch.width);
-  this.y = random(sketch.height, sketch.height * 2);
-  this.vx = 0;
-  this.vy = -random(1, 10) / 5;
-  this.radius = this.baseRadius = 1;
-  this.maxRadius = 50;
-  this.threshold = 150;
-  this.hue = random(180, 240);
-};
+  // Particle Constructor
+  let Particle = function () {
+    this.x = random(sketch.width);
+    this.y = random(sketch.height, sketch.height * 2);
+    this.vx = 0;
+    this.vy = -random(1, 10) / 5;
+    this.radius = this.baseRadius = 1;
+    this.maxRadius = 50;
+    this.threshold = 150;
+    this.hue = random(180, 240);
+  };
 
-// Particle Prototype
-Particle.prototype = {
-  update: function () {
-    var dist, distx, disty, radius;
-    // Determine Distance From Mouse
-    distx = this.x - sketch.mouse.x;
-    disty = this.y - sketch.mouse.y;
-    dist = sqrt(distx * distx + disty * disty);
+  // Particle Prototype
+  Particle.prototype = {
+    update: function () {
+      let radius;
+      // Determine Distance From Mouse
+      let distx = this.x - sketch.mouse.x;
+      let disty = this.y - sketch.mouse.y;
+      let dist = sqrt(distx * distx + disty * disty);
 
-    // Set Radius
-    if (dist < this.threshold) {
-      radius =
-        this.baseRadius +
-        ((this.threshold - dist) / this.threshold) * this.maxRadius;
-      this.radius = radius > this.maxRadius ? this.maxRadius : radius;
-    } else {
-      this.radius = this.baseRadius;
-    }
+      // Set Radius
+      if (dist < this.threshold) {
+        radius =
+          this.baseRadius +
+          ((this.threshold - dist) / this.threshold) * this.maxRadius;
+        this.radius = radius > this.maxRadius ? this.maxRadius : radius;
+      } else {
+        this.radius = this.baseRadius;
+      }
 
-    // Adjust Velocity
-    this.vx += (random(100) - 50) / 1e3;
-    this.vy -= random(1, 20) / 1e4;
+      // Adjust Velocity
+      this.vx += (random(100) - 50) / 1e3;
+      this.vy -= random(1, 20) / 1e4;
 
-    // Apply Velocity
-    this.x += this.vx;
-    this.y += this.vy;
+      // Apply Velocity
+      this.x += this.vx;
+      this.y += this.vy;
 
-    // Check Bounds
-    if (
-      this.x < -this.maxRadius ||
-      this.x > sketch.width + this.maxRadius ||
-      this.y < -this.maxRadius
-    ) {
-      this.x = random(sketch.width);
-      this.y = random(sketch.height + this.maxRadius, sketch.height * 2);
-      this.vx = 0;
-      this.vy = -random(1, 10) / 5;
-    }
-  },
-  render: function () {
-    sketch.beginPath();
-    sketch.arc(this.x, this.y, this.radius, 0, TWO_PI);
-    sketch.closePath();
-    // sketch.fillStyle = 'hsla(' + this.hue + ', 60%, 40%, .35)';
-    // sketch.fill();
-    sketch.stroke();
-  },
-};
+      // Check Bounds
+      if (
+        this.x < -this.maxRadius ||
+        this.x > sketch.width + this.maxRadius ||
+        this.y < -this.maxRadius
+      ) {
+        this.x = random(sketch.width);
+        this.y = random(sketch.height + this.maxRadius, sketch.height * 2);
+        this.vx = 0;
+        this.vy = -random(1, 10) / 5;
+      }
+    },
+    render: function () {
+      sketch.beginPath();
+      sketch.arc(this.x, this.y, this.radius, 0, TWO_PI);
+      sketch.closePath();
+      // sketch.fillStyle = 'hsla(' + this.hue + ', 60%, 40%, .35)';
+      // sketch.fill();
+      sketch.stroke();
+    },
+  };
 
-// Create Particles
-let z = 500;
-while (z--) particles.push(new Particle());
+  // Create Particles
+  let z = 500;
+  while (z--) particles.push(new Particle());
 
-// Sketch Clear
-sketch.clear = () => sketch.clearRect(0, 0, sketch.width, sketch.height);
+  // Sketch Clear
+  sketch.clear = () => sketch.clearRect(0, 0, sketch.width, sketch.height);
 
-// Sketch Update
-sketch.update = function () {
-  let i = particles.length;
-  let results = [];
-  while (i--) results.push(particles[i].update());
-  return results;
-};
+  // Sketch Update
+  sketch.update = function () {
+    let i = particles.length;
+    let results = [];
+    while (i--) results.push(particles[i].update());
+    return results;
+  };
 
-// Sketch Draw
-sketch.draw = function () {
-  let i = particles.length;
-  let results = [];
-  while (i--) results.push(particles[i].render());
-  return results;
-};
+  // Sketch Draw
+  sketch.draw = function () {
+    let i = particles.length;
+    let results = [];
+    while (i--) results.push(particles[i].render());
+    return results;
+  };
 
-}
-
-
-let draw = () => {
-  nrg.setContext(myCanvas.value.getContext('2d'))
 }
 
 onMounted(() => parts())
 
-onUpdated(() => draw())
+onUpdated(() => parts())
 
 </script>
 
-
-<template>
-  <canvas ref='myCanvas' id='canvas1'></canvas>
-</template>
-
-<style scoped>
-#canvas1{
-    width: 95vw; 
-    height: 94vh; 
-    border: 1px solid green;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%)
-  }
-</style>

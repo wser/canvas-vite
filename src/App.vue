8d8c70005bc8b5af77dc79d53b8d7028d7256853
wser <w.web.dev@gmail.com>
2021-11-08 09:19:20 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Game from './components/Game.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Game msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
